dc39186774ee1055b3a5029a1d6b3067cde0fb4b
SathishSaminathan <sathishcse1975@gmail.com>
2025-07-26 10:46:11 +0530

    Refactor AddAccount component and update API endpoint for account management

--- src/pages/accounts/add-account/index.js
+++ src/pages/accounts/add-account/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Row,
   Col,
@@ -6,26 +6,25 @@ import {
   Button,
   Form,
   Select,
-  InputNumber,
   Drawer,
+  Tooltip,
+  Checkbox,
 } from "antd";
+import { InfoCircleOutlined } from "@ant-design/icons";
 import { useSelector, useDispatch } from "react-redux";
 import { putApi } from "redux/sagas/putApiSaga";
 import {
   ACCOUNT_INDUSTRY,
   ACCOUNT_TYPE,
   API_STATUS,
-  CATEGORIES,
-  CUSTOMER_TYPE,
-  GST_TREATMENT,
   OWNERSHIP,
-  PLACE_OF_SUPPLY,
   RATING,
 } from "constants/app-constants";
 import { SERVER_IP } from "assets/Config";
 import { postApi } from "redux/sagas/postApiDataSaga";
 import { resetApiStatus } from "redux/reducers/globals/globalActions";
-import { Checkbox } from "antd";
+
+const { TextArea } = Input;
 
 const AddAccount = ({
   accountAddModal,
@@ -35,104 +34,99 @@ const AddAccount = ({
   refreshList,
   handleClose,
 }) => {
-  const [gstTreatment, setGstTreatment] = React.useState(
-    GST_TREATMENT[0]?.value
-  );
   const [form] = Form.useForm();
   const globalRedux = useSelector((state) => state.globalRedux);
   const dispatch = useDispatch();
-  const { TextArea } = Input;
-
-  const isConsumer = React.useMemo(
-    () => gstTreatment === GST_TREATMENT[0]?.value,
-    [gstTreatment]
-  );
-
-  useEffect(() => {
-    if (isConsumer) {
-      form.setFieldsValue({ gstin: "" });
-    }
-  }, [gstTreatment, form, isConsumer]);
+  const [sameAsBilling, setSameAsBilling] = useState(false);
 
   useEffect(() => {
     if (editAccount) {
-      form.setFieldsValue({
-        category: editAccount?.category || "Individual",
-        displayName: editAccount?.displayName,
-        mobile: editAccount?.mobile,
-        secondaryMobile: editAccount?.secondaryMobile,
-        openingBalance: editAccount?.openingBalance,
-        email: editAccount?.email,
-        panCard: editAccount?.panCard,
-        aadharCard: editAccount?.aadharCard,
-        gstTreatment: editAccount?.gstTreatment || GST_TREATMENT[0]?.value,
-        gstin: editAccount?.gstin,
-        placeOfSupply: editAccount?.placeOfSupply || PLACE_OF_SUPPLY[0],
-        addressLine1: editAccount?.billingDetails?.[0]?.addressLine1,
-        addressLine2: editAccount?.billingDetails?.[0]?.addressLine2,
-        city: editAccount?.billingDetails?.[0]?.city,
-        pincode: editAccount?.billingDetails?.[0]?.pincode,
-      });
-      setGstTreatment(editAccount?.gstTreatment);
+      form.setFieldsValue(editAccount);
     } else {
-      form?.resetFields();
+      form.resetFields();
     }
   }, [editAccount, form]);
 
   const handleSubmit = (values) => {
-    let data = {
+    const payload = {
+      ...values,
       orgId: globalRedux?.selectedOrganization?.id,
-      type: CUSTOMER_TYPE[2] || "",
-      category: values?.category,
-      displayName: values?.displayName || "",
-      email: values?.email || "",
-      mobile: values?.mobile || "",
-      secondaryMobile: values?.secondaryMobile || "",
-      panCard: values?.panCard || "",
-      aadharCard: values?.aadharCard || "",
-      gstTreatment: values?.gstTreatment || "",
-      gstin: values?.gstin || "",
-      openingBalance: values?.openingBalance || 0,
-      billingDetails: [
-        {
-          addressLine1: values?.addressLine1 || "",
-          addressLine2: values?.addressLine2 || "",
-          city: values?.city || "",
-          pincode: values?.pincode || "",
-        },
-      ],
-      placeOfSupply: values?.placeOfSupply || "",
-      remarks: values?.remarks || "",
     };
 
     if (!editAccount) {
-      dispatch(postApi(data, "ADD_LEAD"));
+      dispatch(postApi(payload, "ADD_ACCOUNT_BOOK"));
     } else {
-      let url = `${SERVER_IP}customer/${editAccount._id}?orgId=${globalRedux?.selectedOrganization?.id}`;
-      dispatch(putApi(data, "EDIT_LEAD", url));
+      const url = `${SERVER_IP}account`;
+      dispatch(putApi(payload, "EDIT_ACCOUNT_BOOK", url));
     }
   };
 
   useEffect(() => {
     if (
-      globalRedux.apiStatus.ADD_LEAD === "SUCCESS" ||
-      globalRedux.apiStatus.EDIT_LEAD === "SUCCESS"
+      globalRedux.apiStatus.ADD_ACCOUNT_BOOK === "SUCCESS" ||
+      globalRedux.apiStatus.EDIT_ACCOUNT_BOOK === "SUCCESS"
     ) {
-      dispatch(resetApiStatus(editAccount ? "EDIT_LEAD" : "ADD_LEAD"));
+      dispatch(resetApiStatus(editAccount ? "EDIT_ACCOUNT_BOOK" : "ADD_ACCOUNT_BOOK"));
       refreshList?.();
       handleClose?.();
       form?.resetFields();
+      setSameAsBilling(false);
     }
   }, [globalRedux.apiStatus]);
 
   const loading =
-    globalRedux.apiStatus.ADD_LEAD === API_STATUS.PENDING ||
-    globalRedux.apiStatus.EDIT_LEAD === API_STATUS.PENDING;
+    globalRedux.apiStatus.ADD_ACCOUNT_BOOK === API_STATUS.PENDING ||
+    globalRedux.apiStatus.EDIT_ACCOUNT_BOOK === API_STATUS.PENDING;
+
+  const renderLabelWithTooltip = (label, tooltip) => (
+    <span>
+      {label} <Tooltip title={tooltip}><InfoCircleOutlined /></Tooltip>
+    </span>
+  );
+
+  const handleCopyBillingToShipping = () => {
+    const {
+      billing_street,
+      billing_city,
+      billing_state,
+      billing_zip_code,
+      billing_country,
+    } = form.getFieldsValue([
+      "billing_street",
+      "billing_city",
+      "billing_state",
+      "billing_zip_code",
+      "billing_country",
+    ]);
+
+    form.setFieldsValue({
+      shipping_street: billing_street,
+      shipping_city: billing_city,
+      shipping_state: billing_state,
+      shipping_zip_code: billing_zip_code,
+      shipping_country: billing_country,
+    });
+  };
+
+  const handleShippingChange = () => {
+    if (sameAsBilling) setSameAsBilling(false);
+  };
+
+  const handleResetShipping = () => {
+    form.setFieldsValue({
+      shipping_street: "",
+      shipping_city: "",
+      shipping_state: "",
+      shipping_zip_code: "",
+      shipping_country: "",
+    });
+    setSameAsBilling(false);
+  }
 
   return (
     <Drawer
       placement="right"
-      title={`${editAccount ? "Edit" : "New"} Lead`}
+      title={`${editAccount ? "Edit" : "New"} Account`}
       width={width}
       open={accountAddModal}
       closable
@@ -142,225 +136,225 @@ const AddAccount = ({
     >
       <Form
         form={form}
-        name="add-Account"
         layout="vertical"
+        name="add-account"
         onFinish={handleSubmit}
-        initialValues={{
-          category: "Individual",
-          gstTreatment,
-          placeOfSupply: PLACE_OF_SUPPLY[0],
+        scrollToFirstError={{
+          behavior: "smooth",
+          block: "center",
+          inline: "center",
         }}
       >
-        <div style={{ fontWeight: "bold", marginBottom: 16 }}>
-          Account Information
-        </div>
-
+        <div style={{ fontWeight: "bold", marginBottom: 16 }}>Account Information</div>
         <Row gutter={16}>
           <Col span={12}>
-            <Form.Item label="Account Owner" name="accountowner">
-              <Select placeholder="Select Account owner">
-                {CATEGORIES.map((type) => (
-                  <Select.Option key={type} value={type}>
-                    {type}
-                  </Select.Option>
-                ))}
-              </Select>
+            <Form.Item
+              label={renderLabelWithTooltip("Account Owner", "Person responsible for this account")}
+              name="account_owner_name"
+              rules={[{ required: true, message: "Please enter account owner name" }]}
+            >
+              <Input placeholder="e.g., Roshan" />
             </Form.Item>
           </Col>
           <Col span={12}>
-            <Form.Item label="Rating" name="rating">
+            <Form.Item
+              label={renderLabelWithTooltip("Rating", "Internal engagement score")}
+              name="rating"
+              rules={[{ required: true, message: "Please select a rating" }]}
+            >
               <Select placeholder="Select rating">
-                {RATING.map((type) => (
-                  <Select.Option key={type} value={type}>
-                    {type}
-                  </Select.Option>
+                {RATING.map((r) => (
+                  <Select.Option key={r} value={r}>{r}</Select.Option>
                 ))}
               </Select>
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item
-              label="Account Name"
-              name="accountname"
-              rules={[{ required: true, message: "Required!" }]}
+              label={renderLabelWithTooltip("Account Name", "Registered name of the account")}
+              name="account_name"
+              rules={[{ required: true, message: "Please enter account name" }]}
             >
-              <Input placeholder="Enter account name" />
+              <Input placeholder="e.g., Roshan Enterprises" />
             </Form.Item>
           </Col>
           <Col span={12}>
-            <Form.Item label="Phone" name="phone">
-              <Input placeholder="Enter phone number" />
+            <Form.Item
+              label={renderLabelWithTooltip("Phone", "Primary contact number")}
+              name="phone"
+              rules={[
+                { required: true, message: "Please enter phone number" },
+                { pattern: /^\d{10}$/, message: "Phone number must be 10 digits" }
+              ]}
+            >
+              <Input placeholder="e.g., 9876543210" />
             </Form.Item>
           </Col>
           <Col span={12}>
-            <Form.Item label="Account site" name="account site">
-              <Input placeholder="Enter account site" />
+            <Form.Item label="Account Site" name="account_site">
+              <Input placeholder="e.g., Head Office - Chennai" />
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item label="Fax" name="fax">
-              <Input />
+              <Input placeholder="e.g., 044-12345678" />
             </Form.Item>
           </Col>
-        </Row>
-
-        <Row gutter={16}>
           <Col span={12}>
-            <Form.Item label="Parent Account" name="parentaccount">
-              <Input />
+            <Form.Item label="Parent Account" name="parent_account">
+              <Input placeholder="e.g., Acme Corp" />
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item label="Website" name="website">
-              <Input />
+              <Input placeholder="e.g., www.company.com" />
             </Form.Item>
           </Col>
           <Col span={12}>
-            <Form.Item label="Account Number" name="accountnumber">
-              <Input />
+            <Form.Item label="Account Number" name="account_number">
+              <Input placeholder="e.g., ACC-1001" />
             </Form.Item>
           </Col>
           <Col span={12}>
-            <Form.Item label="Ticker Symbol" name="tickersymbol">
-              <Input/>
+            <Form.Item label="Ticker Symbol" name="ticker_symbol">
+              <Input placeholder="e.g., ROSH" />
             </Form.Item>
           </Col>
-        </Row>
-
-        <Row gutter={16}>
           <Col span={12}>
-            <Form.Item label="Account Type" name="accounttype">
-              <Select>
+            <Form.Item label="Account Type" name="account_type">
+              <Select placeholder="Select account type">
                 {ACCOUNT_TYPE.map((type) => (
-                  <Select.Option key={type} value={type}>
-                    {type}
-                  </Select.Option>
+                  <Select.Option key={type} value={type}>{type}</Select.Option>
                 ))}
               </Select>
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item label="Ownership" name="ownership">
-              <Select>
+              <Select placeholder="Select ownership type">
                 {OWNERSHIP.map((type) => (
-                  <Select.Option key={type} value={type}>
-                    {type}
-                  </Select.Option>
+                  <Select.Option key={type} value={type}>{type}</Select.Option>
                 ))}
               </Select>
             </Form.Item>
           </Col>
-        </Row>
-
-        <Row gutter={16}>
           <Col span={12}>
             <Form.Item label="Industry" name="industry">
               <Select placeholder="Select industry">
-                {ACCOUNT_INDUSTRY.map((type) => (
-                  <Select.Option key={type} value={type}>
-                    {type}
-                  </Select.Option>
+                {ACCOUNT_INDUSTRY.map((industry) => (
+                  <Select.Option key={industry} value={industry}>{industry}</Select.Option>
                 ))}
               </Select>
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item label="Employees" name="employees">
-              <Input />
+              <Input placeholder="e.g., 150" />
             </Form.Item>
           </Col>
           <Col span={12}>
-            <Form.Item label="Annual Revenue" name="annualrevenue">
-              <Input />
+            <Form.Item label="Annual Revenue" name="annual_revenue">
+              <Input placeholder="e.g., 1000000" />
             </Form.Item>
           </Col>
           <Col span={12}>
-            <Form.Item label="SIC code" name="siccode">
-             <Input />
+            <Form.Item label="SIC Code" name="sic_code">
+              <Input placeholder="e.g., 3711" />
             </Form.Item>
           </Col>
         </Row>
-        <div style={{ fontWeight: "bold", marginBottom: 16 }}>
-          Address Information
-        </div>
 
-        <Row gutter={16}>
-          <Col span={12}>
-            <Form.Item label="Billing Street" name="billingstreet">
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label="Shipping Street" name="shippingstreet">
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label="Billing City" name="billingcity">
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label="Shipping City" name="shippingcity">
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label="Billing State" name="billingstate">
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label="Shipping State" name="shippingstate">
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label="Billing Code" name="billingcode">
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label="Shipping Code" name="shippingcode">
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label="Billing Country" name="billingcountry">
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label="Shipping Country" name="shippingcountry">
-              <Input />
-            </Form.Item>
-          </Col>
-        </Row>
-        <div style={{ fontWeight: "bold", marginBottom: 16, marginTop: 16 }}>
-          Description Information
-        </div>
-        <Form.Item label="Description" name="description">
-          <TextArea placeholder="Description" />
-        </Form.Item>
+        <div style={{ fontWeight: "bold", margin: "24px 0 8px" }}>Address Information</div>
 
         <Form.Item>
-          <Row
-            gutter={16}
-            justify="space-between"
-            style={{ marginTop: 16, marginBottom: 16 }}
+          <Checkbox
+            checked={sameAsBilling}
+            onChange={(e) => {
+              const checked = e.target.checked;
+              setSameAsBilling(checked);
+              if (checked) handleCopyBillingToShipping();
+            }}
           >
-            <Col span={12}>
-              <Button danger block onClick={() => setAccountAddModal(false)}>
-                Cancel
-              </Button>
-            </Col>
-            <Col span={12}>
-              <Button type="primary" htmlType="submit" loading={loading} block>
-                {editAccount ? "Update" : "Save"}
-              </Button>
-            </Col>
-          </Row>
+            Shipping address same as billing address
+          </Checkbox>
         </Form.Item>
+
+        {!sameAsBilling && (
+          <Button type="dashed" size="small" onClick={handleResetShipping}>
+            Reset Shipping Address from Billing
+          </Button>
+        )}
+
+        <Row gutter={16} style={{ marginTop: 12 }}>
+          <Col span={12}>
+            <Form.Item label="Billing Street" name="billing_street">
+              <Input placeholder="e.g., 123 MG Road" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Shipping Street" name="shipping_street">
+              <Input placeholder="e.g., 456 Anna Nagar" onChange={handleShippingChange} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Billing City" name="billing_city">
+              <Input placeholder="e.g., Chennai" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Shipping City" name="shipping_city">
+              <Input placeholder="e.g., Coimbatore" onChange={handleShippingChange} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Billing State" name="billing_state">
+              <Input placeholder="e.g., Tamil Nadu" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Shipping State" name="shipping_state">
+              <Input placeholder="e.g., Kerala" onChange={handleShippingChange} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Billing Zip Code" name="billing_zip_code">
+              <Input placeholder="e.g., 600097" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Shipping Zip Code" name="shipping_zip_code">
+              <Input placeholder="e.g., 641001" onChange={handleShippingChange} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Billing Country" name="billing_country">
+              <Input placeholder="e.g., India" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Shipping Country" name="shipping_country">
+              <Input placeholder="e.g., India" onChange={handleShippingChange} />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <div style={{ fontWeight: "bold", marginTop: 24 }}>Description</div>
+        <Form.Item name="description">
+          <TextArea rows={4} placeholder="Add description or notes for follow-up" />
+        </Form.Item>
+
+        <Row gutter={16} style={{ marginTop: 24 }}>
+          <Col span={12}>
+            <Button danger block onClick={() => setAccountAddModal(false)}>
+              Cancel
+            </Button>
+          </Col>
+          <Col span={12}>
+            <Button type="primary" htmlType="submit" loading={loading} block>
+              {editAccount ? "Update" : "Save"}
+            </Button>
+          </Col>
+        </Row>
       </Form>
     </Drawer>
   );
--- src/redux/endpoints.js
+++ src/redux/endpoints.js
@@ -37,7 +37,7 @@ const API_ENDPOINTS = {
 	ADD_INVOICE: `${SERVER_IP}invoice`,
 	ADD_TICKET: `${SERVER_IP}ticket`,
 	ADD_ORGANIZATION: `${SERVER_IP}organization`,
-	ADD_ACCOUNT_BOOK: `${SERVER_IP}accbook`,
+	ADD_ACCOUNT_BOOK: `${SERVER_IP}account`,
 	ADD_SALES_PERSON: `${SERVER_IP}salesperson`,
 
 	// Item Group APIS
